41e9097a9c4ad9755b9edfcb1d853a2eb613ced6
Mia T <mia.mcduffie.13@gmail.com>
2026-02-27 13:18:28 +0000

    Refactor hotel and mountain pages to fetch and display data dynamically

--- next-react-project/src/app/hotels/page.js
+++ next-react-project/src/app/hotels/page.js
@@ -1,9 +1,36 @@
-export default function Page() {
-  return (
-    <main>
-      <div>
-        <h1>Hotel Details</h1>
-      </div>
-    </main>
-  );
+import Image from "next/image";
+
+async function getData() {
+    const res = await fetch("https://snowtooth-api-rest.fly.dev");
+    return res.json();
+}
+
+function HotelBlock({ id, name, capacity }) {
+    return (
+        <div>
+            <h2>{name}</h2>
+            <p>Capacity: {capacity}</p>
+            <Image src={id} width={300} height={300} />
+        </div>
+    );
+}
+
+export default async function Page() {
+    const data = await getData();
+    return (
+        <main>
+            <div>
+                <h1>Hotel Name</h1>
+                <div>
+                    {data.map((hotel) => (
+                        <HotelBlock
+                            key={hotel.id}
+                            name={hotel.name}
+                            capacity={hotel.capacity}
+                        />
+                    ))}
+                </div>
+            </div>
+        </main>
+    );
 }
